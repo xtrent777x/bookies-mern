9873eaae4a4d95d82139679137f1522ccb4d6c99
Trent Roesbery <xtrent777x@gmail.com>
2021-10-29 07:58:31 -0600

    mutations create, login, save, delete

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,1 +1,52 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!){
+        login(email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser(
+        $username: String!,
+        $email: String!,
+        $password: String!
+    ) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook( $savedBook: SavedBooks ){
+        saveBook( savedBook: $savedBook ){
+            authors
+            description
+            bookId
+            image
+            title
+            link
+        }
+    }
+`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: String){
+        deleteBook(bookId: $bookId){
+            _id
+            username
+            savedBooks
+        }
+    }
+`;
